3aac6fae1f402d5fd7cde6cbb414787fa8caaaa3
Berke Yapıcı <berke.yapiciii@gmail.com>
2023-02-27 19:36:15 +0300

    registration

---
+++ conditionals.js
@@ -0,0 +1,7 @@
+var not = 40 
+  if(not>50){
+     console.log('geçtiniz')
+ }
+  if(not<=50){
+    console.log('kaldınız')
+ }
---
+++ conditionals2.js
@@ -0,0 +1,6 @@
+var sayi1 = 10
+var sayi2 = 10
+var sayi3 = 10
+if(sayi1==sayi2 || sayi1==sayi3){ // && = ve anlamında || = veya anlamında
+    console.log("Eşit")
+}    
